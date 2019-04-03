4043118634c8e05213788c7cda6e3e4d726ba905
Sahruzzaman Riyad <sahruzzaman01@gmail.com>
2019-04-03 10:21:30 +0600

    MainMethod Created

---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Please Input two double value");
+        double FV = Double.parseDouble(args[0]);
+        double SV = Double.parseDouble(args[1]);
+        
+    }
+}
