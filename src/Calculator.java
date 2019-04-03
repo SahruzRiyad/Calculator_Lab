c5795c2d0b93ad22c81f5097700e27adfb39ea00
Farhin Mashiat Mayabee <f.m.maya833@gmail.com>
2019-04-03 10:39:07 +0600

    multiplyMethod was added

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,6 +3,13 @@ public class Calculator {
         System.out.println("Please Input two double value");
         double FV = Double.parseDouble(args[0]);
         double SV = Double.parseDouble(args[1]);
+
         
     }
+    double multiMethod(double firstValue , double secondValue)
+    {
+        double result = firstValue * secondValue;
+        return result;
+    }
+
 }
